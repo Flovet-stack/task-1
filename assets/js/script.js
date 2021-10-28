19ec9776937cfaca0c7965b45527c3ca389c3850
Flovet-stack <flovetoloveto@yahoo.com>
2021-10-28 17:20:16 +0100

    finnished the price plan filter functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,51 @@
-// Creating the hero tabs functionality 
-let contents = document.getElementsByClassName('content-sub');
+// Creating the hero tabs functionality
+let contents = document.getElementsByClassName("content-sub");
 contents = Array.from(contents);
-let tabs = document.getElementsByClassName('tab');
+let tabs = document.getElementsByClassName("tab");
 tabs = Array.from(tabs);
-let contentReset = () => { //function to hide all the contents
-    contents.forEach(content => {
-        content.style.display = 'none';
-    })
+let contentReset = () => {
+  //function to hide all the contents
+  contents.forEach((content) => {
+    content.style.display = "none";
+  });
 };
-let showContent = (target, content) => { //function to show specific targeted content
-    tabs.forEach(tab => {
-        tab.classList.remove('active');
-    });
-    contentReset(); //call the reset function to hide all the contents 
-    target.classList.add('active');
-    content.classList.add('fade-in');
-    content.style.display = 'block';
+let showContent = (target, content) => {
+  //function to show specific targeted content
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  contentReset(); //call the reset function to hide all the contents
+  target.classList.add("active");
+  content.classList.add("fade-in");
+  content.style.display = "block";
 };
 // show specific content on click
-tabs.forEach(tab => {
-    tab.addEventListener('click', (e) => {
-        const content = document.getElementById(e.target.getAttribute('data-to'));
-        showContent(e.target, content);
-    });
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const content = document.getElementById(e.target.getAttribute("data-to"));
+    showContent(e.target, content);
+  });
 });
 
-
-// creating the pricing plan filter 
-const plan = {
-    basic: {},
-    premium: {},
-}
+// creating the pricing plan filter
+const monthly = document.getElementById("monthly");
+const annually = document.getElementById("annually");
+let toggler = document.getElementById("plan-toggler");
+toggler.addEventListener("click", () => {
+  if (toggler.classList.contains("toggle")) {
+    toggler.classList.remove("toggle");
+  } else {
+    toggler.classList.add("toggle");
+  } //change toggler animation class
+  if(toggler.classList.contains('toggle')) {
+      monthly.style.display = 'none';
+      annually.style.display = 'block';
+      annually.children[1].style.display = 'block';
+      annually.children[0].children[0].children[0].classList.add('active');
+  } else {
+      annually.style.display = "none";
+      monthly.style.display = "block";
+      monthly.children[1].style.display = 'block'
+      monthly.children[0].children[0].children[0].classList.add('active');
+  }
+});
